1027a67a720a047fcf4bd398591b83ea745d9404
Ash Kestrel <tetrapteryxgames@gmail.com>
2016-01-18 17:47:50 -0700

    book list

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var knex = require('./db/knex');
+require('dotenv').load();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
